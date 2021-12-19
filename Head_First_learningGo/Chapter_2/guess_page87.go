06c0efb8d0c25dfc7bfef87a512a371609c8bc4e
Gulenok Valentin <gulenok.valentin@yandex.ru>
2021-12-20 00:50:44 +0600

    Go-training-aka-Golang-
    Head First Go, a brain friendly guide. ...page 125

--- Head_First_learningGo/Chapter_2/guess_page87.go
+++ Head_First_learningGo/Chapter_2/guess_page87.go
@@ -1,10 +1,54 @@
-// Игра отгадай число Page 89
+// guess - игра, в которой игрок должен угадать случайное число. Page 89 - page 107
 package main
 
-import "math/rand"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
 
 func main() {
+	second := time.Now().Unix()
+	rand.Seed(second)
 	target := rand.Intn(100) + 1
+	fmt.Println("I've chosen a random number between 1 and 100.")
+	fmt.Println("Can you guess it?")
+	//fmt.Println(target)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	success := false
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10-guesses, "guesses left.")
+		fmt.Println("Make a quess: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW!")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH!")
+		} else {
+			success = true
+			fmt.Println("Good job! You guessed it!")
+			break
+		}
+	}
+	if !success {
+		fmt.Println("Sorry. You didn't guess my number. It was:", target)
+	}
 }
 
 // package main
